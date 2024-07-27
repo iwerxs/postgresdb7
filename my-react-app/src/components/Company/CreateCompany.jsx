788756d245ee2290695be1534754126cfd7dc64f
iwerxs <iworxs@gmail.com>
2024-07-27 10:28:48 +0100

    FE updated create company label

--- my-react-app/src/components/Company/CreateCompany.jsx
+++ my-react-app/src/components/Company/CreateCompany.jsx
@@ -1,11 +1,11 @@
-import { useState } from 'react';
-import axios from 'axios';
-import '../styles.css';
-import Companies from './Companies';
+import { useState } from "react";
+import axios from "axios";
+import "../styles.css";
+import Companies from "./Companies";
 
 const CreateCompany = () => {
   const [company, setCompany] = useState({
-    name: ''
+    name: "",
   });
 
   const handleChange = (e) => {
@@ -15,28 +15,38 @@ const CreateCompany = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      await axios.post('http://localhost:8080/create-company', company);
-      alert('Company created successfully');
+      await axios.post("http://localhost:8080/create-company", company);
+      alert("Company created successfully");
       setCompany({
-        name: ''
+        name: "",
       });
     } catch (error) {
-      console.error('Error creating company:', error);
-      alert('Failed to create company');
+      console.error("Error creating company:", error);
+      alert("Failed to create company");
     }
   };
 
   return (
-    <div>
-      <h2>Create Company</h2>
-      <form onSubmit={handleSubmit}>
-        <label>Company Name:</label>
-        <input type="text" name="name" value={company.name} onChange={handleChange} required />
-        <br />
-        <button type="submit">Create Company</button>
-      </form>
-      <Companies/>
-    </div>
+    <>
+      <div>
+        <h2>Create Company</h2>
+        <form onSubmit={handleSubmit}>
+          <label className='create-company-heading'>New Company Name:</label>
+          <input
+            type='text'
+            name='name'
+            value={company.name}
+            onChange={handleChange}
+            required
+          />
+          <br />
+          <button type='submit'>Create Company</button>
+        </form>
+      </div>
+      <div>
+        <Companies />
+      </div>
+    </>
   );
 };
 
